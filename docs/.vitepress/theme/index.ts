8651625064b137c0bde6b59a2b565b10bc4daf1e
phb <phb@newpermarket.com>
2026-09-21 17:34:23 +0900

    feat: 상세 페이지 사용자 글 영역 추가

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,10 +2,14 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import AuthStatus from './AuthStatus.vue'
+import DetailCommunity from './detail-community/DetailCommunity.vue'
 import './custom.css'
 
 export default {
   extends: DefaultTheme,
+  enhanceApp({ app }) {
+    app.component('DetailCommunity', DetailCommunity)
+  },
   Layout() {
     return h(DefaultTheme.Layout, null, {
       'nav-bar-content-after': () => h(AuthStatus),
